8f6aeb65ee797926829234003283c08fc94d8cc4
anonymous.dev <anonymous.dev@gmail.com>
2026-07-19 00:42:40 +0700

    migrate response store to model

--- core-common-starter/src/main/java/com/hit/common/mapper/DomainMapper.java
+++ core-common-starter/src/main/java/com/hit/common/mapper/DomainMapper.java
@@ -1,23 +1,16 @@
 package com.hit.common.mapper;
 
-import org.mapstruct.IterableMapping;
-import org.mapstruct.Named;
-
 import java.util.Collection;
 import java.util.List;
 
 public interface DomainMapper<E, M> {
 
-    @Named("toEntity")
     E toEntity(M model);
 
-    @IterableMapping(qualifiedByName = "toEntity")
     List<E> toEntities(Collection<M> models);
 
-    @Named("toModel")
     M toModel(E entity);
 
-    @IterableMapping(qualifiedByName = "toModel")
     List<M> toModels(Collection<E> entities);
 
 }
